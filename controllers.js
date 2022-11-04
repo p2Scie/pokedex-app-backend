e6e68640fcfa962b6f433b5864b304240aef61ee
Stevens <sdemervil@gmail.com>
2022-11-04 18:37:59 +0100

    create show route

--- controllers.js
+++ controllers.js
@@ -8,53 +8,66 @@ const index = (req, res) => {
     .catch((err) => res.status(500, { message: 'Request Failed' }));
 };
 
+const show = (req, res) => {
+  const { id } = req.body;
+
+  fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    .then((response) => response.json())
+    .then((json) => res.send(json))
+    .catch((err) => res.status(500, { message: 'Request Failed' }));
+};
+
 const add = async (req, res) => {
-    const {name, weight, height} = req.body;
+  const { name, weight, height } = req.body;
 
-    const pokemon = await Pokemon.findOne({ name: name });
-    if(pokemon) res.status(500).send({message: "this pokemon already exists"});
+  const pokemon = await Pokemon.findOne({ name: name });
+  if (pokemon) res.status(500).send({ message: 'this pokemon already exists' });
 
-    Pokemon.create({ name, weight, height, comment: "" })
+  Pokemon.create({ name, weight, height, comment: '' })
     .then((pokemon) => res.send(pokemon))
     .catch((err) => res.status(500, { message: 'Request Failed' }));
-}
+};
 
 const edit = (req, res) => {
-    const {id, name, weight, height, comment }= req.body;
+  const { id, name, weight, height, comment } = req.body;
 
-    const data = {name, weight, height, comment};
-  
-    Pokemon.findByIdAndUpdate(id, data, (err, pokemon) => {
-      if (err) {
-        res.status(500, { message: 'Une erreur est survenu' });
-        throw err;
-      }
+  const data = { name, weight, height, comment };
 
-      res.status(200, {
-        message: "Les information ont bien été mis à jour !",
-      }).send(pokemon);
-    }) 
-}
+  Pokemon.findByIdAndUpdate(id, data, (err, pokemon) => {
+    if (err) {
+      res.status(500, { message: 'Une erreur est survenu' });
+      throw err;
+    }
+
+    res
+      .status(200, {
+        message: 'Les information ont bien été mis à jour !',
+      })
+      .send(pokemon);
+  });
+};
 
 const remove = (req, res) => {
-    const {id} = req.body
+  const { id } = req.body;
 
-    Pokemon.findByIdAndDelete(id, (err, pokemon) => {
-        if (err) res.status(500).json({err, message: "Unable to delete user"})
-        if (pokemon === null) res.status(404).json({message: "pokemon not found"}) 
-        else res.status(200).json({message: "pokemon deleted", pokemon})
-    })
-}
+  Pokemon.findByIdAndDelete(id, (err, pokemon) => {
+    if (err) res.status(500).json({ err, message: 'Unable to delete user' });
+    if (pokemon === null)
+      res.status(404).json({ message: 'pokemon not found' });
+    else res.status(200).json({ message: 'pokemon deleted', pokemon });
+  });
+};
 
 const favorites = async (req, res) => {
-    const items = await Pokemon.find();
-    res.status(201).send(items);
-}
+  const items = await Pokemon.find();
+  res.status(201).send(items);
+};
 
 module.exports = {
   index,
   add,
   favorites,
   edit,
-  remove
+  remove,
+  show,
 };
--- index.js
+++ index.js
@@ -9,13 +9,14 @@ app.use(bodyParser.json());
 const database = require('./database');
 
 /****** ROUTES ******/
-const { index, add, favorites, edit, remove } = require('./controllers');
+const { index, add, favorites, edit, remove, show } = require('./controllers');
 
 app.get('/', index);
 app.post('/add', add);
 app.get('/favorites', favorites);
 app.put('/edit', edit);
 app.delete('/delete', remove);
+app.get('/show', show);
 
 
 app.listen(port, () => {
