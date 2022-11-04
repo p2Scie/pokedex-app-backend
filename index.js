53f3c8dee3ff015a35eff21eeb86744369f2b4db
Stevens <sdemervil@gmail.com>
2022-11-04 14:17:09 +0100

    return pokemon list from the base route

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+const { json } = require('express');
 const express = require('express');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    fetch('https://pokeapi.co/api/v2/ability')
+    .then((response) => response.json())
+    .then((json) => res.send(json)) 
+    .catch((err) => res.status(500, {message: 'Request Failed'}));
 });
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
