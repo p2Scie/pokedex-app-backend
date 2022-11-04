0663266f798a6d77373a0e58936ab88dfdbfb91d
Stevens <sdemervil@gmail.com>
2022-11-04 14:33:14 +0100

    update pokemon api rul

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    fetch('https://pokeapi.co/api/v2/ability')
+  fetch('https://pokeapi.co/api/v2/pokemon')
     .then((response) => response.json())
-    .then((json) => res.send(json)) 
-    .catch((err) => res.status(500, {message: 'Request Failed'}));
+    .then((json) => res.send(json))
+    .catch((err) => res.status(500, { message: 'Request Failed' }));
 });
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
